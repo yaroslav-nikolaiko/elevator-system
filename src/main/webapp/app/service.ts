ce499b85919b717c58f405b00495ce788212c2e4
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2016-01-31 19:09:10 +0200

    refactor UI

--- src/main/webapp/app/service.ts
+++ src/main/webapp/app/service.ts
@@ -6,10 +6,12 @@ import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class Service {
+    private floorUrl = "api/floors";
+    private elevatorUrl = "api/elevators";
     constructor (private http: Http) {}
 
     getFloors() {
-        return this.http.get("api/floors")
+        return this.http.get(this.floorUrl)
             .map(res => <Floor[]> res.json())
             .catch(this.logAndPassOn);
     }
@@ -17,18 +19,18 @@ export class Service {
     updateFloor(floor: Floor){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        this.http.put("api/floors", JSON.stringify(floor), {headers : headers});
+        this.http.put(this.floorUrl, JSON.stringify(floor), {headers : headers})
+            .catch(this.logAndPassOn)
+            .subscribe(()=>console.log("Level "+floor.level+" updated"));
     }
 
     getElevators() {
-        return this.http.get("api/elevators")
+        return this.http.get(this.elevatorUrl)
             .map(res => <Elevator[]> res.json())
             .catch(this.logAndPassOn);
     }
 
     private logAndPassOn (error: Error) {
-        // in a real world app, we may send the server to some remote logging infrastructure
-        // instead of just logging it to the console
         console.error(error);
         return Observable.throw(error);
     }
